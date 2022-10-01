73068982ea6e4e4843f15c9dda847e8524bd1ad5
Dislate <ice_balyk@mail.ru>
2022-10-01 19:32:09 +0300

    Counters

---
+++ collections/Counter objects/main.py
@@ -0,0 +1,27 @@
+from collections import Counter
+
+if __name__ == "__main__":
+    c1 = Counter("anysequence")
+    print(c1)  # Counter({'e': 3, 'n': 2, 'a': 1, 'y': 1, 's': 1, 'q': 1, 'u': 1, 'c': 1})
+    c2 = Counter({
+        'a': 1,
+        "c": 1,
+        "e": 3,
+    })
+    c3 = Counter(
+        a=1,
+        c=1,
+        e=3,
+    )
+    print(c2)
+    print(c3)
+
+    ct = Counter()
+    print(ct)  # empty Counter objects
+    ct.update('abca')
+    print(ct)  # Counter({'a': 2, 'b': 1, 'c': 1})
+    ct.update({'a': 3})
+    print(ct)  # Counter({'a': 5, 'b': 1, 'c': 1})
+
+    for item in ct:
+        print(item, ct[item])
